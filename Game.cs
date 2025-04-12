461fadceff6dc0d66fca065fa9139fdad74dad73
imbelfort <belfort.ishmael25@outlook.com>
2025-04-11 23:15:53 -0400

    Agregar archivos de proyecto.

---
+++ Game.cs
@@ -0,0 +1,64 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+
+namespace LetraT
+{
+    public class Game : GameWindow
+    {
+        private Escenario escenario;
+
+        public Game(int width, int height) : base(width, height, GraphicsMode.Default, "Diseño Letra T - 3D")
+        {
+            this.escenario = new Escenario(new Vector3(0, 0, 0));
+            escenario = Serializador.DeserializarObjeto<Escenario>("escenario.json");
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+            float aspectRatio = (float)Width / Height;
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(
+                MathHelper.DegreesToRadians(45.0f), aspectRatio, 0.1f, 100.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            
+            // Configura la cámara
+            Matrix4 modelview = Matrix4.LookAt(
+                new Vector3(0.0f, 0.0f, 4.5f), // Posición de la cámara
+                new Vector3(0.0f, 0.1f, 0.0f), // Punto de mira
+                Vector3.UnitY); // Vector arriba
+            GL.LoadMatrix(ref modelview);
+            
+            escenario.dibujar(new Vector3(0, 0, 0));
+
+            SwapBuffers();
+        }
+    }
+}
---
+++ Serializador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LetraT
+{
+    public class Serializador
+    {
+        public static void SerializarObjeto<T>(T objeto, string rutaArchivo)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(objeto, Formatting.Indented);
+                File.WriteAllText(rutaArchivo, json);
+                Console.WriteLine($"Objeto serializado y guardado en el archivo: {rutaArchivo}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al serializar el objeto: {ex.Message}");
+            }
+        }
+
+        public static T DeserializarObjeto<T>(string rutaArchivo)
+        {
+            try
+            {
+                string json = File.ReadAllText(rutaArchivo);
+                T objeto = JsonConvert.DeserializeObject<T>(json);
+                Console.WriteLine($"Objeto deserializado desde el archivo: {rutaArchivo}");
+                return objeto;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al deserializar el objeto: {ex.Message}");
+                return default(T);
+            }
+        }
+    }
+}
